e3d3e582d088b1a62551f46c73ee36e51d729d51
Tyra-hans <tyrahans17@gmail.com>
2019-10-05 18:44:19 +0300

    Created a class user that will allow the userto sign up. Also created a test file for the user class to use red-green refactor method

--- passlock.py
+++ passlock.py
@@ -3,4 +3,8 @@ class User:
     Class that allows users to sign up and create a 
     password and username for themselves
     '''
-    
+    sign_up_info = []
+    def __init__(self,username,password):
+
+        self.username = username
+        self.password = password
